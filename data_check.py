11015f5fc821503a4e3ee143384a2b4877cac7f3
yathridv <yathridv@yahoo.com>
2023-02-18 16:01:06 -0500

    data check

---
+++ data_check.py
@@ -0,0 +1,36 @@
+# Print Hello World!
+
+
+print('Hello World!')
+
+
+# List of Cities
+
+citys =["boston", "cincinati", "chicago","nashville"]
+print(citys)
+print(citys[3])
+print(citys[1], citys[3])
+print(citys [2], citys [3])
+
+
+# My Car Dictionary
+
+car_dictionary ={"brand":"Honda",
+"modal": "CR-V",
+"year": 1990,
+"colors": ["red", "white", "silver"]
+}
+print(car_dictionary)
+print(car_dictionary["modal"])
+print(car_dictionary ["year"], car_dictionary ["colors"])
+print(car_dictionary ["brand"], car_dictionary["modal"], car_dictionary["colors"], car_dictionary["year"])
+
+
+# My groceries tuple
+
+Mytuple = ( {"fruits": ['apple', 'mango', 'banana'] }, {"Dollars" : [10, 20, 30] } , 'pennies', 'veggies' )
+print(Mytuple[1])
+print(Mytuple[0])
+
+
+
